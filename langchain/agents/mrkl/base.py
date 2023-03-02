2d00c0225f8d2fd06cf05c82b2c63fe5234e0e73
John Shahbazian <jshahbazi@gmail.com>
2023-03-02 08:02:27 -0500

    React to Action = None as the final answer

--- langchain/agents/mrkl/base.py
+++ langchain/agents/mrkl/base.py
@@ -43,7 +43,12 @@ def get_action_and_input(llm_output: str) -> Tuple[str, str]:
     regex = r"Action: (.*?)\nAction Input: (.*)"
     match = re.search(regex, llm_output, re.DOTALL)
     if not match:
-        raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        if re.search("Action:", llm_output, re.DOTALL):
+            action = llm_output.split(FINAL_ANSWER_ACTION)[-1].strip()
+            if action == "None":
+                return "Final Answer", llm_output.split("Action:")[0]
+        else:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
     action = match.group(1).strip()
     action_input = match.group(2)
     return action, action_input.strip(" ").strip('"')
